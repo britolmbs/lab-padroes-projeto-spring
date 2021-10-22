b49eaefc0e01d8f2f11b55e4302134f70430030b
britolmbs <britolmbs@hotmail.com>
2021-10-22 14:52:53 -0300

    Codigo do projeto

---
+++ src/main/java/one/digitalinnvation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,24 @@
+package one.digitalinnvation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr
+ * Os seguites módulos foram selecionados:
+ *  - Spring Data JPA
+ *  - Spring Web
+ *  - H2 Database
+ *  OpenFign
+ * @author Lucas
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
